4d5d90a5df9193216f8b2958ba48aac97327fb0f
jtmengel <me@jtmengel.com>
2015-09-27 10:52:30 -0600

    SCALA adding a simple sample script file to be used as a template for future scripts

---
+++ scala/SampleShellScript.scala
@@ -0,0 +1,11 @@
+#!/bin/sh
+exec scala "$0" "$@"
+!#
+
+object SampleShellScript{
+  def main(args: Array[String]) = {
+  println(s"Hello World, ${args.mkString("::")}");
+  }
+}
+
+SampleShellScript.main(args)
